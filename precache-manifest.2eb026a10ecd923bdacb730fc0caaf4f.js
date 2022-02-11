71460e1298349355de08d8feeb23d85b12bb1857
guiufo <guiufo@users.noreply.github.com>
2022-02-11 20:22:48 +0000

    deploy: 53e2e338afc81d484b7466ea5548d5a78dd2de72

---
+++ precache-manifest.2eb026a10ecd923bdacb730fc0caaf4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1b80e253f2a78a96e66785884d7f6c5",
+    "url": "https://guiufo.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "0b7b1b404ce10e2bec8b",
+    "url": "https://guiufo.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "bb6c4316862bbe792d6b",
+    "url": "https://guiufo.github.io/task-management-frontend/static/js/2.3065c8fa.chunk.js"
+  },
+  {
+    "revision": "0b7b1b404ce10e2bec8b",
+    "url": "https://guiufo.github.io/task-management-frontend/static/js/main.71d86a37.chunk.js"
+  },
+  {
+    "revision": "0f8c4e6d80e290c612bc",
+    "url": "https://guiufo.github.io/task-management-frontend/static/js/runtime~main.39bc5ed4.js"
+  }
+]);
